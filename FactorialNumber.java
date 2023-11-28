571e0898d40d45fcd31bbbb84250c115490929cf
Anurag Sharma <91717812+Anurag262001@users.noreply.github.com>
2023-11-29 02:20:16 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,10 @@
+class FactorialNumber{
+    public static void main(String[] args) {
+        int number = 3;
+        int sum = 1;
+        for(int i=1;i<=3;i++){
+        sum = sum*i;
+        }
+        System.out.println(sum);
+    }    
+}
